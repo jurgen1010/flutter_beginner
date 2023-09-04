cb4e928e0d7bf5e4045ca4be14f73348ba125aa8
Jurgen Perez <jurgen1010@hotmail.com>
2023-09-04 12:50:02 -0500

    Tipos de variables y mapas en Dart

---
+++ 02_variable_types.dart
@@ -0,0 +1,29 @@
+void main() {
+  final String superheroe = 'batmam';
+  final int edad = 35;
+  final bool isVillain = false;
+
+//Listas
+  final List<String> abilities = ['sigilo'];
+  final sprites = <String>['batman/front.png', 'batman/back.png'];
+
+//Dinamic: cualquier tipo de dato, es como un comodin, acepta nullos
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5, 6];
+  errorMessage = {1, 2, 3, 4, 5, 6}; //Sets de datos
+  errorMessage = () => true; //puede ser funcion
+  errorMessage = null;
+
+
+//String multilinea
+  print("""
+
+  $superheroe
+  $edad
+  $isVillain
+  $abilities
+  $sprites
+  $errorMessage
+  """);
+}
