a7f9ea3b93c76dd734566a729d95380712c1169e
Jurgen <jurgen1010@hotmail.com>
2023-09-07 11:47:28 -0500

    Parametros por nombre

--- 05_funciones_parametros.dart
+++ 05_funciones_parametros.dart
@@ -2,6 +2,8 @@ main() {
   print(greetEveryone());
 
   print('Suma: ${addTwoNumbers(10, 40)}');
+
+  print(greetPerson(name: 'Jurgen', message: 'Hi'));
 }
 
 // funcion flecha no tiene cuerpo { .... }
@@ -16,10 +18,15 @@ int addTwoNumbers(int a, int b) {
 //Ahora addTwoNumbers en funcion tipo flecha (lambda funcion)
 int addTwoNumbers(int a, int b) => a + b;
 
-//elvolvemos el parametro con  [  ]
-int addTwoNumbersOptional(int a, [ int b = 0 ]) {
-
+//envolvemos el parametro con  [  ]
+int addTwoNumbersOptional(int a, [int b = 0]) {
 //  b ??= 0; forma en la que podemos asignar valor de 0 por defecto cuando no es definido
 
   return a + b;
+}
+
+// Como recibir parametros sin orden especifico, envolermos los params en {  }
+
+String greetPerson({required String name, String message = 'Hola'}) {
+  return '$message $name ';
 }
